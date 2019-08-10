5e5dc7e3d1e5f61445395a2c92b1278db01e1194
jatin sandilya <jatinsandilya@scootsys-MacBook-Pro.local>
2019-08-10 17:27:42 +0530

    Add location detection

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from "react-scroll";
-import  FireEmoji  from "../images/fire5.webp"
+import FireEmoji from "../images/fire5.webp"
 
 import Nav from "./nav";
 
 class Header extends Component {
+
+  getLocation = () => {
+
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        alert("Lat : " + position.coords.latitude + " Long: " + position.coords.longitude);
+
+        return;
+      })
+    }
+    else {
+      console.info("geolocation is not supported in this environment");
+    }
+
+  }
+
   render() {
     return (
 
       <header>
         <Nav />
         <div className="head">
-          <h1>Best of Halal <img src={FireEmoji} style={{ height: '63px',    width: '54px' }} />< br /> <span>Near You.</span></h1>
+          <h1>Best of Halal <img src={FireEmoji} style={{ height: '63px', width: '54px' }} />< br /> <span>Near You.</span></h1>
           <div>
             <div> <Link className="contact" to="discover" smooth={true} spy={true} activeClass="active" offset={-70}
               duration={500} >Get Started</Link> </div>
+            <div>
+              <a className="locateme" 
+                    onClick={this.getLocation}> 
+                    <i class="fas fa-map-marker-alt"></i>  Locate Me
+              </a>
+            </div>
           </div>
         </div>
 
