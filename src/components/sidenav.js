ce859291078df07fb3188ff0f7e74e0637b2ce7a
jatin sandilya <jatinsandilya@scootsys-mbp.bundl.in>
2019-08-13 14:31:28 +0530

    Add scroll to sidenav

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -1,27 +1,31 @@
 import React, { useState, Fragment } from 'react';
-import useWindowScrollPosition from "@rehooks/window-scroll-position";
 
+import { Link } from "react-scroll";
 
 
 function SideNav() {
 
-        
+
     function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("mySidenav").style.width = "0";
     }
 
 
-  return (
-    <Fragment>
-        <div id="mySidenav" className="sidenav">
-            <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&times;</a>
-            <a href="#">Discover</a>
-            <a href="#">About</a>
-            <a href="#">Mission</a>
-            <a href="#">Contact</a>
-        </div>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <div id="mySidenav" className="sidenav">
+                <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&times;</a>
+                <Link to="discover" smooth={true} spy={true} activeClass="active" offset={-70}
+                    duration={500} onClick={closeNav} >Discover</Link>
+                <Link to="about" smooth={true} spy={true} activeClass="active" offset={-70}
+                    duration={500} onClick={closeNav} >About</Link>
+                <Link to="mission" smooth={true} spy={true} activeClass="active" offset={-70}
+                    duration={500} onClick={closeNav}>Mission</Link>
+                <Link to="contact" smooth={true} spy={true} activeClass="active" offset={-70}
+                    duration={500} onClick={closeNav}>Contact</Link>
+            </div>
+        </Fragment>
+    );
 }
 
 
