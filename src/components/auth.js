08b760b8dc47dfc2e50973ec0b16c880dff227b4
jatin sandilya <jatinsandilya@scootsys-MacBook-Pro.local>
2019-08-25 23:49:07 +0530

    Add redirection for route

--- src/components/auth.js
+++ src/components/auth.js
@@ -13,6 +13,8 @@ const responseGoogle = (response) => {
 
 
 const redirect = (props) => {
+    console.log("redirect")
+    console.log(props)
     if(props.isAuthenticated){
         props.history.push("/arena");
         return;
@@ -34,7 +36,7 @@ class Auth extends Component {
                 <GoogleLogin
                     clientId="1098012249427-811i7d4t6f17837buv7dmmeqh7lfmqmb.apps.googleusercontent.com"
                     buttonText="Login with Google To Continue"
-                    onSuccess={ () => this.props.signIn(redirect) }
+                    onSuccess={this.props.signIn}
                     theme="dark"
                     onFailure={responseGoogle}
                     cookiePolicy={'single_host_origin'}
@@ -50,7 +52,7 @@ const AuthDecorator = withRouter((props) => {
     if (props.match.path === "/auth" && !props.isAuthenticated)
         return (
             <div>
-                <Auth signIn={props.signIn} />
+                <Auth signIn={()=> props.history.push("/arena") && props.signIn() } />
             </div>
         )
     else
