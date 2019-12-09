a2d2195e458ba6998a3e4acdf0915812d7ebde30
Jatin Sandilya <jatinsandilya@Jatins-MacBook-Air.local>
2019-12-10 01:41:09 +0530

    Updates

---
+++ precache-manifest.1033568d2f029667ac68df5aae1e00eb.js
@@ -0,0 +1,322 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1922cec985e897e9fa6",
+    "url": "/React-Landing-Template/static/css/main.d2a54526.chunk.css"
+  },
+  {
+    "revision": "b1922cec985e897e9fa6",
+    "url": "/React-Landing-Template/static/js/main.b1922cec.chunk.js"
+  },
+  {
+    "revision": "943d1b4771d1b0c4a051",
+    "url": "/React-Landing-Template/static/css/1.61ec147c.chunk.css"
+  },
+  {
+    "revision": "943d1b4771d1b0c4a051",
+    "url": "/React-Landing-Template/static/js/1.943d1b47.chunk.js"
+  },
+  {
+    "revision": "94367ffa6d09f0d0dfac",
+    "url": "/React-Landing-Template/static/js/runtime~main.94367ffa.js"
+  },
+  {
+    "revision": "fc6f90eae5b11066b4b8a37277cef8d6",
+    "url": "/React-Landing-Template/static/media/analog.fc6f90ea.png"
+  },
+  {
+    "revision": "3e41ece56d1abe6253f602463ccf2bbc",
+    "url": "/React-Landing-Template/static/media/digital.3e41ece5.jpg"
+  },
+  {
+    "revision": "bb0773dfe9ba738b02a6f599e4a0eeb0",
+    "url": "/React-Landing-Template/static/media/circuit.bb0773df.gif"
+  },
+  {
+    "revision": "7ef0ecb05e1021eb3377ae3a0cfda306",
+    "url": "/React-Landing-Template/static/media/ti.7ef0ecb0.png"
+  },
+  {
+    "revision": "5b32ce7e8f6fce6f74429d8043ab7ce6",
+    "url": "/React-Landing-Template/static/media/pause.5b32ce7e.svg"
+  },
+  {
+    "revision": "b5d832b3f0cea900f68bb744ce44e37f",
+    "url": "/React-Landing-Template/static/media/backward.b5d832b3.svg"
+  },
+  {
+    "revision": "06d1a095e1d97c17878b0bf114ebc20c",
+    "url": "/React-Landing-Template/static/media/forward.06d1a095.svg"
+  },
+  {
+    "revision": "140bf5320c0ddc7df3d2b25ba961ca0c",
+    "url": "/React-Landing-Template/static/media/back.140bf532.svg"
+  },
+  {
+    "revision": "13b598f821912ab5d4660fc68e673f2c",
+    "url": "/React-Landing-Template/static/media/options.13b598f8.svg"
+  },
+  {
+    "revision": "c84559fa271aaea61ac21b85cceed4bf",
+    "url": "/React-Landing-Template/static/media/play.c84559fa.svg"
+  },
+  {
+    "revision": "4d8fdeb265ff6d34fb3bd8e4292665c0",
+    "url": "/React-Landing-Template/static/media/AvenirNext-Regular.4d8fdeb2.4d8fdeb2.ttf"
+  },
+  {
+    "revision": "1a826d47792aea86be79af21f0a9fd7a",
+    "url": "/React-Landing-Template/static/media/chip_3.1a826d47.png"
+  },
+  {
+    "revision": "c1ff9b44a55d68265ed457581c38e514",
+    "url": "/React-Landing-Template/static/media/replay.c1ff9b44.svg"
+  },
+  {
+    "revision": "eecf58820e73b41098f6261f7cf5f4f1",
+    "url": "/React-Landing-Template/static/media/landing-page.eecf5882.png"
+  },
+  {
+    "revision": "0f81767054165bd9f44f96ab6a7964a7",
+    "url": "/React-Landing-Template/static/media/landing-page 2.0f817670.png"
+  },
+  {
+    "revision": "6b196a14ff373e2248d888bd13659dc2",
+    "url": "/React-Landing-Template/static/media/tick.6b196a14.gif"
+  },
+  {
+    "revision": "3d8245dcb4489694a6a263b05c1cca01",
+    "url": "/React-Landing-Template/static/media/KaTeX_AMS-Regular.3d8245dc.woff2"
+  },
+  {
+    "revision": "c67be87adba7d31c013be127b936233d",
+    "url": "/React-Landing-Template/static/media/KaTeX_AMS-Regular.c67be87a.ttf"
+  },
+  {
+    "revision": "ac1d46d953d403677171697581a284d2",
+    "url": "/React-Landing-Template/static/media/KaTeX_AMS-Regular.ac1d46d9.woff"
+  },
+  {
+    "revision": "970d3e76493b82fccf21ad5888ddee77",
+    "url": "/React-Landing-Template/static/media/KaTeX_Caligraphic-Bold.970d3e76.woff2"
+  },
+  {
+    "revision": "3f61a84d76e80396489d32bc9dd8d444",
+    "url": "/React-Landing-Template/static/media/KaTeX_Caligraphic-Bold.3f61a84d.ttf"
+  },
+  {
+    "revision": "445f96a387df0d13ded71f27c608516d",
+    "url": "/React-Landing-Template/static/media/KaTeX_Caligraphic-Bold.445f96a3.woff"
+  },
+  {
+    "revision": "74f6918c7d2b768ffd32048102bc0172",
+    "url": "/React-Landing-Template/static/media/KaTeX_Caligraphic-Regular.74f6918c.woff"
+  },
+  {
+    "revision": "c3bc8fcec0e85a50cabf71e4e8074991",
+    "url": "/React-Landing-Template/static/media/KaTeX_Caligraphic-Regular.c3bc8fce.ttf"
+  },
+  {
+    "revision": "0ef0f2e356a2e1c457b6585d34edae77",
+    "url": "/React-Landing-Template/static/media/KaTeX_Caligraphic-Regular.0ef0f2e3.woff2"
+  },
+  {
+    "revision": "950649ba5e5cfd37cdad74095411d350",
+    "url": "/React-Landing-Template/static/media/KaTeX_Fraktur-Bold.950649ba.woff2"
+  },
+  {
+    "revision": "1aca7ef7f976fb54a207ffc8aa180e38",
+    "url": "/React-Landing-Template/static/media/KaTeX_Fraktur-Bold.1aca7ef7.woff"
+  },
+  {
+    "revision": "135ccd74931753318f6f52f3fce19018",
+    "url": "/React-Landing-Template/static/media/KaTeX_Fraktur-Regular.135ccd74.woff2"
+  },
+  {
+    "revision": "c5b430bfcb3e5423b77401afcdb69b66",
+    "url": "/React-Landing-Template/static/media/KaTeX_Fraktur-Regular.c5b430bf.woff"
+  },
+  {
+    "revision": "e11e6bf02fc41279a540d3655abf3b07",
+    "url": "/React-Landing-Template/static/media/KaTeX_Fraktur-Bold.e11e6bf0.ttf"
+  },
+  {
+    "revision": "a9509497466d16e6b7265a223ea39093",
+    "url": "/React-Landing-Template/static/media/KaTeX_Fraktur-Regular.a9509497.ttf"
+  },
+  {
+    "revision": "0ba875a1d548ebfdd00c7594022e01b7",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-Bold.0ba875a1.woff"
+  },
+  {
+    "revision": "c5b67fb3f6bdaa7f2dd75e5bc8d929eb",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-Bold.c5b67fb3.woff2"
+  },
+  {
+    "revision": "aaaa1b761b7cc9d5e10b85f30731b878",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-Bold.aaaa1b76.ttf"
+  },
+  {
+    "revision": "0719833c23aeba83786a29e84a165875",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-BoldItalic.0719833c.woff2"
+  },
+  {
+    "revision": "5aeca883bc41b522fdf1409ee912b51c",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-BoldItalic.5aeca883.woff"
+  },
+  {
+    "revision": "bdbadb27e05fb8e51da903db59f90d77",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-BoldItalic.bdbadb27.ttf"
+  },
+  {
+    "revision": "bb7c45db8908c8fb88a293895a64e018",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-Italic.bb7c45db.woff"
+  },
+  {
+    "revision": "6bf616283a81e40e4ac755883862b472",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-Italic.6bf61628.woff2"
+  },
+  {
+    "revision": "29b27903a08a71f8171be21ba413d4dd",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-Regular.29b27903.woff2"
+  },
+  {
+    "revision": "2155d7897af94033a6b6ed44133b867e",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-Italic.2155d789.ttf"
+  },
+  {
+    "revision": "7a31741a44e58952cb4b8a763c206fcd",
+    "url": "/React-Landing-Template/static/media/KaTeX_Math-Italic.7a31741a.woff2"
+  },
+  {
+    "revision": "8eb56b3ff5b141cd3732a24e65c2b339",
+    "url": "/React-Landing-Template/static/media/KaTeX_Math-Italic.8eb56b3f.woff"
+  },
+  {
+    "revision": "1fd21713706a2b70960faf21b1fa4e26",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-Regular.1fd21713.woff"
+  },
+  {
+    "revision": "7b8d1fa0668e317c4a2d34399e74af25",
+    "url": "/React-Landing-Template/static/media/KaTeX_Main-Regular.7b8d1fa0.ttf"
+  },
+  {
+    "revision": "e38d0f6844b9520510715fd989bdd5bd",
+    "url": "/React-Landing-Template/static/media/KaTeX_SansSerif-Bold.e38d0f68.woff2"
+  },
+  {
+    "revision": "84299b016163b0ae5c776604aa1cb726",
+    "url": "/React-Landing-Template/static/media/KaTeX_SansSerif-Bold.84299b01.woff"
+  },
+  {
+    "revision": "10740d7488d690a743339650103f1cb3",
+    "url": "/React-Landing-Template/static/media/KaTeX_Math-Italic.10740d74.ttf"
+  },
+  {
+    "revision": "9123667cef81279761df5cd62ec8c46e",
+    "url": "/React-Landing-Template/static/media/KaTeX_SansSerif-Bold.9123667c.ttf"
+  },
+  {
+    "revision": "1057b70228cb6936e7714e7817db6ce2",
+    "url": "/React-Landing-Template/static/media/KaTeX_SansSerif-Italic.1057b702.woff2"
+  },
+  {
+    "revision": "95c5a3d43efbcb8c14184d00a7c5eed4",
+    "url": "/React-Landing-Template/static/media/KaTeX_SansSerif-Italic.95c5a3d4.woff"
+  },
+  {
+    "revision": "f9c440abdd25c5a954d855516fed39f9",
+    "url": "/React-Landing-Template/static/media/KaTeX_SansSerif-Italic.f9c440ab.ttf"
+  },
+  {
+    "revision": "f9e06d59e70de05c5f0b7f7f65c14565",
+    "url": "/React-Landing-Template/static/media/KaTeX_SansSerif-Regular.f9e06d59.woff2"
+  },
+  {
+    "revision": "35bda414c6c2f75940b086de18f42e72",
+    "url": "/React-Landing-Template/static/media/KaTeX_SansSerif-Regular.35bda414.woff"
+  },
+  {
+    "revision": "2eae604dbbc4b2c0ceede62c61c9e8df",
+    "url": "/React-Landing-Template/static/media/KaTeX_SansSerif-Regular.2eae604d.ttf"
+  },
+  {
+    "revision": "ae1fad1f4d1c227c9d567da8ea9f988c",
+    "url": "/React-Landing-Template/static/media/KaTeX_Script-Regular.ae1fad1f.woff2"
+  },
+  {
+    "revision": "60febfa114c5e32f0ce73050476aa39f",
+    "url": "/React-Landing-Template/static/media/KaTeX_Script-Regular.60febfa1.woff"
+  },
+  {
+    "revision": "fda0824e8cfb676664ad063c367cfce3",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size1-Regular.fda0824e.woff2"
+  },
+  {
+    "revision": "e9169ca7b32608b6235ec9ffff742a71",
+    "url": "/React-Landing-Template/static/media/KaTeX_Script-Regular.e9169ca7.ttf"
+  },
+  {
+    "revision": "1d6cf6b89f694dc76e13ecc4e8214dd2",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size1-Regular.1d6cf6b8.woff"
+  },
+  {
+    "revision": "f2e296ef7cef0f6f16912e5d171929b2",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size1-Regular.f2e296ef.ttf"
+  },
+  {
+    "revision": "a7a0eb93afb696cb9b4efb87e6cd45a3",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size2-Regular.a7a0eb93.woff2"
+  },
+  {
+    "revision": "f48e83b7bcaa68a86a78e8edfee1a04e",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size2-Regular.f48e83b7.woff"
+  },
+  {
+    "revision": "397bd6872be7adff4f000c49fa85a9c3",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size3-Regular.397bd687.woff2"
+  },
+  {
+    "revision": "0767ede65042583bcc06f09055f4d7ca",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size2-Regular.0767ede6.ttf"
+  },
+  {
+    "revision": "0387ab387ace32f15b43c3a4a39f187f",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size3-Regular.0387ab38.woff"
+  },
+  {
+    "revision": "882f723427a0986846fe786bd8d6f6c7",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size3-Regular.882f7234.ttf"
+  },
+  {
+    "revision": "726db3bd435c8a6e45e44741dae67263",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size4-Regular.726db3bd.woff"
+  },
+  {
+    "revision": "3c14ed11dd8eea981c93bf283193856d",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size4-Regular.3c14ed11.woff2"
+  },
+  {
+    "revision": "f43e86963e9cfda37bb2e958ddedcaad",
+    "url": "/React-Landing-Template/static/media/KaTeX_Typewriter-Regular.f43e8696.woff2"
+  },
+  {
+    "revision": "27901d5ce93d8971c416d9123fedb911",
+    "url": "/React-Landing-Template/static/media/KaTeX_Size4-Regular.27901d5c.ttf"
+  },
+  {
+    "revision": "ba01f72a8db89265149f3f712dc7f9a3",
+    "url": "/React-Landing-Template/static/media/KaTeX_Typewriter-Regular.ba01f72a.woff"
+  },
+  {
+    "revision": "6b9645af6e119fd5d85f24e21044ed03",
+    "url": "/React-Landing-Template/static/media/KaTeX_Typewriter-Regular.6b9645af.ttf"
+  },
+  {
+    "revision": "658170868ea4db8b053e08ffcf167d98",
+    "url": "/React-Landing-Template/static/media/settings.65817086.svg"
+  },
+  {
+    "revision": "adc356033d7d8bcb46c07f0c28cd21e3",
+    "url": "/React-Landing-Template/index.html"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('fetch',evt =>{
+    // console.log(evt)
+}) 
+
+self.addEventListener('install',evt =>{
+    // console.log(evt)
+})
+
+self.addEventListener('activate',evt =>{
+    // console.log(evt)
+})
+
+
+
