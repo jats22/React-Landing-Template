f9b749d6605a25a3bcd3ce6651432debfde09c2d
Jatin Sandilya <jatinsandilya@Jatins-MacBook-Air.local>
2019-12-14 17:28:32 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Landing-Template/precache-manifest.57dcaff17887f63b627f98b001f750d6.js"
+  "/React-Landing-Template/precache-manifest.5e519f54fe451e52217de4953fb64d9e.js"
 );
 
 workbox.clientsClaim();
